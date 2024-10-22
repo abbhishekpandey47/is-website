cc3e68efaee3c9e1f9f64b30b13936ba18b50d36
redhairrs <r3hab.rs@gmail.com>
2024-10-22 17:37:39 +0530

    video page fixed

--- services-data/_videoMetadata.js
+++ services-data/_videoMetadata.js
@@ -1,19 +1,5 @@
 const videoMetadata = [
     {
-        vidLink: "https://devzero.b-cdn.net/Devbox%20Xata%20Database%20.mp4",
-        topic: "Devbox Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png",
-        type:"cdn"
-    },
-    {
-        vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20Planetscale%20Database.mov",
-        topic: "how to guide Planetscale Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/how_to_guide_planetscale_database.png",
-        type:"cdn"
-    },
-    {
         vidLink: "https://devzero.b-cdn.net/How%20to%20Connect%20Supabase%20to%20Your%20Dev%20Box-inline-subtitles.mp4",
         topic: "How to Connect Supabase to Your Dev Box-inline-subtitles",
         category: "Editorials",
@@ -21,24 +7,10 @@ const videoMetadata = [
         type:"cdn"
     },
     {
-        vidLink: "https://devzero.b-cdn.net/GCP%20how%20to%20guide%20Cloud%20Run%20Video.mp4",
-        topic: "GCP How to Guide: Cloud Run Video",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/gcp_how_to_guide_cloud_run_video.png",
-        type:"cdn"
-    },
-    {
         vidLink: "https://devzero.b-cdn.net/GCP%20how%20to%20guide%20BigQuery.mp4",
         topic: "GCP How to Guide: BigQuery",
         category:"Editorials",
         imgLink:"/video_thumbnails/gcp_how_to_guide_bigquery.png",
-        type:"cdn"
-    },
-    {
-        vidLink: "https://devzero.b-cdn.net/GCP%20how-to-guide%20Compute%20Instance.mp4",
-        topic: "GCP How to Guide: Compute Instance",
-        category:"Editorials",
-        imgLink:"/video_thumbnails/gcp_how_to_guide_compute_instance.png",
         type:"cdn"
     },
     {
@@ -70,13 +42,6 @@ const videoMetadata = [
         type:"cdn"
     },
     {
-        vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20azure%20vnet.mp4",
-        topic: "How to Guide: Azure VNet",
-        category:"Editorials",
-        imgLink:"/video_thumbnails/how_to_guide_azure_vnet.png",
-        type:"cdn"
-    },
-    {
         ytEmbedLink: "GTMEKQIM84I",
         topic: "How an Ex-Uber Engineer Founded DevZero to Solve Developer Challenges",
         category:"Podcasts",
@@ -95,6 +60,12 @@ const videoMetadata = [
         type:"yt",
         category:"Podcasts",
     },
+    {
+        ytEmbedLink: "erczPxh8iGc",
+        topic: "Drift detection, asset management, and auto-code generation for your cloud infrastructure",
+        type:"yt",
+        category:"Editorials",
+    }
     
 ];
 
