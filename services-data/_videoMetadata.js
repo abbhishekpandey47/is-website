7b562ceac7289fec6f7eaf54971ea8e42f1ef0d9
Ayush Kumar Singh Rathor <ayush.rathor.ug21@nsut.ac.in>
2024-10-22 12:05:19 +0530

    SS added all

--- services-data/_videoMetadata.js
+++ services-data/_videoMetadata.js
@@ -7,69 +7,68 @@ const videoMetadata = [
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20Planetscale%20Database.mov",
-        topic: "Devbox Database",
+        topic: "how to guide Planetscale Database",
         category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        imgLink:"/video_thumbnails/how_to_guide_planetscale_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/How%20to%20Connect%20Supabase%20to%20Your%20Dev%20Box-inline-subtitles.mp4",
-        topic: "Devbox Database",
+        topic: "How to Connect Supabase to Your Dev Box-inline-subtitles",
         category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        imgLink:"/video_thumbnails/how_to_connect_supabase_to_your_dev_box-inline-subtitles.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/GCP%20how%20to%20guide%20Cloud%20Run%20Video.mp4",
-        topic: "Devbox Database",
+        topic: "GCP How to Guide: Cloud Run Video",
         category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        imgLink:"/video_thumbnails/gcp_how_to_guide_cloud_run_video.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/GCP%20how%20to%20guide%20BigQuery.mp4",
-        topic: "Devbox Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        topic: "GCP How to Guide: BigQuery",
+        category:"Editorials",
+        imgLink:"/video_thumbnails/gcp_how_to_guide_bigquery.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/GCP%20how-to-guide%20Compute%20Instance.mp4",
-        topic: "Devbox Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        topic: "GCP How to Guide: Compute Instance",
+        category:"Editorials",
+        imgLink:"/video_thumbnails/gcp_how_to_guide_compute_instance.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20AWS%20ECS.mp4",
-        topic: "Devbox Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        topic: "How to Guide: AWS ECS",
+        category:"Editorials",
+        imgLink:"/video_thumbnails/how_to_guide_aws_ecs.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20aws%20elasti%20cache.mp4",
-        topic: "Devbox Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        topic: "How to Guide: AWS ElastiCache",
+        category:"Editorials",
+        imgLink:"/video_thumbnails/how_to_guide_aws_elasti_cache.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20AWS%20RDS%20.mp4",
-        topic: "Devbox Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        topic: "How to Guide: AWS RDS",
+        category:"Editorials",
+        imgLink:"/video_thumbnails/how_to_guide_aws_rds.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20AWS%20VPC.mp4",
-        topic: "Devbox Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        topic: "How to Guide: AWS VPC",
+        category:"Editorials",
+        imgLink:"/video_thumbnails/how_to_guide_aws_vpc.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20azure%20vnet.mp4",
-        topic: "Devbox Database",
-        category: "Editorials",
-        imgLink:"/video_thumbnails/devbox_database.png"
+        topic: "How to Guide: Azure VNet",
+        category:"Editorials",
+        imgLink:"/video_thumbnails/how_to_guide_azure_vnet.png"
     },
     {
         ytEmbedLink: "GTMEKQIM84I",
         topic: "How an Ex-Uber Engineer Founded DevZero to Solve Developer Challenges",
-        category: "Podcasts",
-
+        category:"Podcasts",
     },
 ];
 
