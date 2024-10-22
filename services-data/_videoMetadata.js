fa866de74c04bdaeefe8b9c18894e4577323d3c0
Ayush Kumar Singh Rathor <ayush.rathor.ug21@nsut.ac.in>
2024-10-22 11:15:52 +0530

    ss added

--- services-data/_videoMetadata.js
+++ services-data/_videoMetadata.js
@@ -2,57 +2,68 @@ const videoMetadata = [
     {
         vidLink: "https://devzero.b-cdn.net/Devbox%20Xata%20Database%20.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20Planetscale%20Database.mov",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/How%20to%20Connect%20Supabase%20to%20Your%20Dev%20Box-inline-subtitles.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/GCP%20how%20to%20guide%20Cloud%20Run%20Video.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/GCP%20how%20to%20guide%20BigQuery.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/GCP%20how-to-guide%20Compute%20Instance.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20AWS%20ECS.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20aws%20elasti%20cache.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20AWS%20RDS%20.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20AWS%20VPC.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         vidLink: "https://devzero.b-cdn.net/how%20to%20guide%20azure%20vnet.mp4",
         topic: "Devbox Database",
-        category: "Editorials"
+        category: "Editorials",
+        imgLink:"/video_thumbnails/devbox_database.png"
     },
     {
         ytEmbedLink: "GTMEKQIM84I",
