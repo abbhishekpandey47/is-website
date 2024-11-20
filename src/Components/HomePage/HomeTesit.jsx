f372826387013a8d68f0d0f0c21b04df3d966550
anuragkainth <anuragkainth07@gmail.com>
2024-11-20 15:04:36 +0530

    fix: home page - testimonial cards

--- src/Components/HomePage/HomeTesit.jsx
+++ src/Components/HomePage/HomeTesit.jsx
@@ -13,7 +13,7 @@ const testiArr = [
         name: "Cindy Blake",
         src: "/Testimon/cindyFirefly.jpg",
         pos: "VP Marketting, Firefly",
-        comment: "Infrasity was quick to onboard and understand how to best show off the capabilities of Firefly's cloud asset management. Team has been super responsive and collaborative..."
+        comment: "Infrasity was quick to onboard and understand how to best show off the capabilities of Firefly's cloud asset management. Team has been super responsive and collaborative."
 
     },
     {
@@ -32,7 +32,7 @@ const testiArr = [
         name: "Frank Weissmann",
         src: "/Testimon/Frank.jpg",
         pos: "Customer Success Lead, firefly.ai",
-        comment: "Infrasity's work has improved the client's SEO, earning a score of over 75%. They'vs also enabled the client to onboard end customers faster. Moreover, the team listens to the client's content needs, produces work that aligns with their conversation and delivers output in a quick turnaround time..."
+        comment: "Infrasity's work has improved the client's SEO, earning a score of over 75%. They'vs also enabled the client to onboard end customers faster. Moreover, the team listens to the client's content needs, produces work that aligns with their conversation and delivers output in a quick turnaround time."
     },
     {
         name: "Igal Zeifman",
@@ -44,27 +44,27 @@ const testiArr = [
         name: "Sri Krishna",
         src: "/Testimon/sriMiddleware.jpeg",
         pos: "Content Head, Middleware",
-        comment: "Infrasity is incredibly responsive and understands client needs exceptionally well, always delivering promptly and as expected. Their attention to detail and outstanding customer support truly set them apart. Communication through email and messaging was seamless, and while the quality of work is top-notch, we look forward to even faster delivery in the future..."
+        comment: "Infrasity is incredibly responsive and understands client needs exceptionally well, always delivering promptly and as expected. Their attention to detail and outstanding customer support truly set them apart. Communication through email and messaging was seamless, and while the quality of work is top-notch, we look forward to even faster delivery in the future."
     }
 ]
 
 const TestiCard = ({ name, sorc, pos, comment, index, carPtr }) => {
     return (
-        <div className={`max-md:w-[350px] h-[55vh] max-lg:h-[40vh]  transition card bg-zinc-950 w-[30%] shadow-xl border-[#edeaea] border-2 flex flex-col overflow-x-hidden`}>
+        <div className={`max-md:w-[350px] h-[55vh] max-lg:h-[40vh] transition card bg-zinc-950 w-[30%] shadow-xl border-[#edeaea] border-2 flex flex-col overflow-x-hidden`}>
             <div className='h-[15%] flex justify-center '>
                 <div className=" pt-8 items-center card-title text-[4em] max-lg:text-[2em]"><svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-magenta-40 | mb-4 mx-auto"><path fill="#fff" fillOpacity="0.01" d="M0 0h24v24H0z"></path><path d="M6.816 6.176c1.232-1.21 2.9-2.193 4.713-2.722V3c-2.973.227-5.8 1.437-7.904 3.328C1.305 8.445 0 11.168 0 14.193c0 1.815.508 3.48 1.378 4.69C2.32 20.318 3.77 21 5.51 21c1.377 0 2.682-.53 3.625-1.437.943-.907 1.45-2.193 1.45-3.706 0-1.21-.362-2.269-1.16-3.176-.653-.832-1.668-1.362-2.828-1.589H4.35c.218-1.815 1.016-3.479 2.466-4.916zm12.471 0c1.233-1.21 2.9-2.193 4.713-2.722V3c-2.973.227-5.8 1.437-7.903 3.328-2.32 2.117-3.626 4.84-3.626 7.865 0 1.815.508 3.48 1.378 4.69C14.79 20.318 16.242 21 17.982 21c1.378 0 2.683-.53 3.625-1.437.943-.907 1.45-2.193 1.45-3.706 0-1.21-.362-2.269-1.16-3.176-.652-.832-1.667-1.362-2.827-1.589h-2.248c.217-1.815 1.015-3.479 2.465-4.916z" fill="currentColor"></path></svg></div>
             </div>
             <div className="card-body max-lg:px-3 max-lg:pb-3 pt-10 overflow-y-auto scrollertesti h-[55%] ">
-                <p className='text-sm max-lg:text-[0.6em] hover:underline cursor-pointer text-center'>{comment}</p>
+                <p className='text-md max-lg:text-[0.7rem] cursor-pointer text-center'>{comment}</p>
                 {/* <div className='flex justify-end mt-2'><h2 className="card-title max-lg:text-[2em] text-[4em] source-serif-4-quote">"</h2></div> */}
             </div>
 
             <div className='card-body pt-0 h-[30%]'>
-                <div className='flex justify-start gap-5 mt-5'>
+                <div className='flex justify-start gap-5 mt-5 items-center'>
                     <div><Image loading='lazy' width={100} height={100} className='max-w-[60px] rounded-[100%]' src={sorc} alt="" /></div>
                     <div className='flex flex-col justify-center gap-1'>
-                        <h2 className='max-lg:text-[0.7em]'>{name}</h2>
-                        <p className='max-lg:text-[0.7em]'>{pos}</p>
+                        <h2 className='max-lg:text-[0.7rem]'>{name}</h2>
+                        <p className='max-lg:text-[0.7rem]'>{pos}</p>
                     </div>
                 </div>
             </div>
@@ -147,7 +147,7 @@ const HomeTesit = () => {
     useEffect(() => {
         newArr = [testiMemo[carPtr]]
         if (window.innerWidth >= 1024) newArr = [testiMemo[carPtr - 1], testiMemo[carPtr], testiMemo[carPtr + 1]]
-        setRenderArr(newArr)
+            setRenderArr(newArr)
         return () => {
 
         }
@@ -161,7 +161,7 @@ const HomeTesit = () => {
                 <div className='flex justify-center text-white pt-20 testiMonialsHead overflow-y-hidden'><h2 className='text-center max-sm:text-[1.6em] text-[2em] w-[80%] max-sm:w-[95%] quicksand-semibold text-[wheat]'>Hear what our customers say about us</h2></div>
                 <div className='flex justify-center text-white testiMonialsHead'><h1 className='text-center text-[1.7em] w-[70%] quicksand-bold max-lg:text-[1.2em] max-sm:text-[.9em] max-sm:w-[95%] pt-3'>We help <span className="specialtext">DevTools and engineering companies</span> drive growth through strategic technical content and developer relations services.</h1></div>
             </div>
-            <div className='flex justify-center gap-6 py-16 quicksand-light flex-wrap w-5/6 m-auto text-white testiMonCardHome'>
+            <div className='flex justify-center gap-6 py-16 quicksand-light flex-wrap w-7/6 m-auto text-white testiMonCardHome'>
                 
                 {
                     renderArr.map((testi, index) => {
