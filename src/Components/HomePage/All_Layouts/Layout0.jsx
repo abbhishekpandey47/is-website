a79d90953312fc94e1a1cb61702523e7e39c1b36
shan-infrasity <shan@infrasity.com>
2024-12-24 17:11:59 +0530

    Update Layout0.jsx

--- src/Components/HomePage/All_Layouts/Layout0.jsx
+++ src/Components/HomePage/All_Layouts/Layout0.jsx
@@ -20,7 +20,7 @@ const Layout0JS = () => {
         <div className="flex pt-12 justify-center flex-row pb-12 Layout0-class w-fit">
             <div className="flex w-[90%] items-center max-lg:flex-col justify-between">
                 <div className="w-[30%] max-lg:w-full max-lg:pl-0 lg:pr-5 text-left max-lg:text-center max-lg:pb-10">
-                    <h1 className="text-3xl mb-4 text-white quicksand-bold"><span className="specialtext">Research.</span></h1>
+                    <h3 className="text-3xl mb-4 text-white quicksand-bold"><span className="specialtext">Research.</span></h3>
                     <p className="text-white max-lg:text-center quicksand-medium">
                         We identify the technical challenges and keywords that matter most to your audience,
                         <br className="max-lg:hidden max-lg" />
