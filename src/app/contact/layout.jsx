a950bde44f33f706ac254a01ec5f3cb782676b75
shan <shan@shans-MacBook-Air.local>
2025-01-02 17:24:06 +0530

    add keywords and meta descriptions

--- src/app/contact/layout.jsx
+++ src/app/contact/layout.jsx
@@ -2,9 +2,9 @@ import Navbar from "@/Components/Navbar/Navbar";
 import Footer from "@/Components/HomePage/Footer";
 
 export const metadata = {
-  title: "Content Marketing Services for SaaS Companies | infrasity",
-  description: "Explore the best SaaS content marketing agencies, including Infrasity, know for driving growth through tailored content strategies for early stage startups.",
-  keywords: "Best SaaS Content Marketing Agencies, B2B SaaS content marketing agency, Technical Marketing Content for Software Startups, Content Marketing Strategy for Technology, A Complete Introduction to Technical Marketing, Content Marketing for Tech Companies, SaaS Blog Writing Services, Content Marketing for Tech Startups",
+  title: "Technical Writing Services for SaaS Startups | Infrasity",
+  description: "Boost your SaaS growth with Infrasity, a B2B tech content marketing agency offering expert technical content writing services tailored for success.",
+  keywords: "Technical Writing Services, technical content writer​, technical content marketing​,tech content marketing,content marketing for tech companies,developer relations, devrel service, technical content writing​,content marketing for tech businesses,b2b tech content marketing agency,technical content writing services​",
   name: "viewport",
   content: "width=device-width, initial-scale=1.0",
   metadataBase: `https://www.infrasity.com`,
@@ -12,13 +12,13 @@ export const metadata = {
     canonical: './',
   },
   openGraph: {
-    title: "Infrasity",
-    description: "Explore the best SaaS content marketing agencies, including Infrasity, know for driving growth through tailored content strategies for early stage startups.",
+    title: "Technical Writing Services for SaaS Startups | Infrasity",
+    description: "Boost your SaaS growth with Infrasity, a B2B tech content marketing agency offering expert technical content writing services tailored for success.",
     url: "",
     type: "website",
     images: [
       {
-        url: "",
+        url: "https://www.infrasity.com/_next/image?url=%2Flogodata%2Finfrasity_logo.png&w=256&q=75",
         width: 1200,
         height: 630,
         alt: "Infrasity - Technical Content Marketing for Cloud Companies",
