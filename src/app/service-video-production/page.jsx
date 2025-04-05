9bc0bbec741cc064821318f9d75cc2c6cb610a4f
Satish Kolhe <kolhesatish96@gmail.com>
2025-04-05 19:22:34 +0530

    Added new colors and line

--- src/app/service-video-production/page.jsx
+++ src/app/service-video-production/page.jsx
@@ -438,7 +438,6 @@ const page = () => {
             #1 AI VIDEO PLATFORM FOR MARKETING
           </span>
         </div>
-
         <div className="quicksand-bold text-[5em] max-sm:text-[4em] tracking-tighter leading-[80px] text-white text-center flex justify-center">
           <h1 className="w-3/4 max-sm:w-[95%] leading-[80px] max-sm:leading-[69px] text-left max-lg:text-center max-lg:mx-auto">
             B2B Video Production for{" "}
