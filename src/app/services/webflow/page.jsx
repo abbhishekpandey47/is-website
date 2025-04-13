75aca2ac5df09c7f64c4782cefcd8317ed9f0cee
dishafaujdar <dishachoudhary1102@gmail.com>
2025-04-13 22:36:50 +0530

    Hero Section

--- src/app/services/webflow/page.jsx
+++ src/app/services/webflow/page.jsx
@@ -1,8 +1,25 @@
 "use client";
 import React, { useMemo } from "react";
 import HomeMarquee from "@/Components/HomePage/HomeMarquee";
+import { motion } from "framer-motion"; 
 import CardMotion from "./cardsmotion";
-import CardComponent from "@/Components/ui/CardComponent";
+import CardComponent from "@/app/services/webflow/CardComponent";
+import AnimatedCard from "@/app/services/webflow/AnimatedCard";
+import CustomerReview from "@/app/services/webflow/CustomerReview";
+import ThirdPartyIntegrations from "./ThirdPartyIntegrations";
+import css1 from "./images/thecss/css4.png";
+import css2 from "./images/thecss/css5.png";
+import WebflowMarquee from "@/app/services/webflow/Webflowmarquee";
+import FAQ from "@/app/services/service-video-production/FAQ";
+import BookDemo from "./bookDemo";
+import TestimonialSlider from "./testimonials";
+import Webtable from "./table";
+import img1 from './images/devs/dev1.png';
+import img2 from './images/devs/dev2.png';
+import img3 from './images/devs/dev3.png';
+import img4 from './images/devs/dev4.png';
+import img5 from './images/devs/dev5.png';
+import img6 from './images/devs/dev6.png';
 
 const fileList = [
   "aviator.png",
@@ -34,67 +51,67 @@ const fileList = [
 
 const serviceArr = [
   {
-    id: "1",
-    head: "Developer Led Tech Content",
-    para: "Our in-house engineers craft highly technical content, including blogs, how-to guides, and whitepapers, by working hands-on with your product. This ensures the utmost technological credibility and delivers deep insights that resonate with engineers and developers.",
+    head: "Webflow Development",
+    para: "Upgrade your online presence with our Webflow Development agency. We create perfect websites that load fast, look great on any device, and are fully optimized for search engines. Boost your digital presence with our custom Webflow development company solutions.",
+    image: img1,
   },
   {
-    id: "2",
-    head: "Video Production",
-    para: "Our in-house engineers works closely with your product hands-on, to craft videos that showcase its true potential. By understanding the tech through and through, we create videos that engage developers and decision-makers, ensuring the highest level of credibility and authenticity.",
+    head: "Website Design",
+    para: "Our Webflow design agency specializes in creating visually stunning websites on the Webflow platform. Our Webflow design studio creates designs that not only capture but also effectively convey your brand's message, carefully optimized for maximum conversion.",
+    image: img2,
   },
   {
-    id: "3",
-    head: "Growth Engineering",
-    para: "As a part of tech content marketing strategy We develop ready-to-use recipe libraries for tech stacks like Node.js, Bun.js, React, and CockroachDB. These libraries provide end users with ready-made boilerplate code, allowing them to jumpstart their projects without having to write code from scratch.",
+    head: "Webflow SEO",
+    para: "Improve your website's visibility with our Webflow SEO services. Our focus is on optimizing your on-page SEO, content marketing, ensuring higher Google rankings, and attracting more traffic to your site. Trust our Webflow agency to enhance your online presence and drive results through effective SEO strategies.",
+    image: img3,
   },
   {
-    id: "1",
-    head: "Developer Led Tech Content",
-    para: "Our in-house engineers craft highly technical content, including blogs, how-to guides, and whitepapers, by working hands-on with your product. This ensures the utmost technological credibility and delivers deep insights that resonate with engineers and developers.",
+    head: "Website Speed Optimization",
+    para: "Supercharge your website's performance with our Webflow agency's Website Speed Optimization services. Our experienced team is dedicated to enhancing your site's speed, optimizing every element for lightning-fast loading. Let us elevate your user experience and ensure your website performs at its peak efficiency.",
+    image: img4,
   },
   {
-    id: "2",
-    head: "Video Production",
-    para: "Our in-house engineers works closely with your product hands-on, to craft videos that showcase its true potential. By understanding the tech through and through, we create videos that engage developers and decision-makers, ensuring the highest level of credibility and authenticity.",
+    head: "Webflow Integrations",
+    para: "Connect your Webflow site with various tools, be it HubSpot, Salesforce, Greenhouse, and more. We've got your integration needs covered.",
+    image: img5,
   },
   {
-    id: "3",
-    head: "Growth Engineering",
-    para: "As a part of tech content marketing strategy We develop ready-to-use recipe libraries for tech stacks like Node.js, Bun.js, React, and CockroachDB. These libraries provide end users with ready-made boilerplate code, allowing them to jumpstart their projects without having to write code from scratch.",
+    head: "Webflow Migration",
+    para: "Our experienced certified Webflow team ensures a seamless migration of your WordPress, Wix, Squarespace, or any other platform to Webflow. We prioritize a smooth transition, perfect SEO optimization, and excellent site speed. Your requirements are carefully followed throughout the migration process.",
+    image: img6,
   },
 ];
 
 const serviceCards = [
   {
-    image: "💰",
+    image: css1,
     title: "Simple Pricing",
-    desc: "We keep straightforward pricing based on what you opt for. No Contracts. Cancel anytime.",
+    desc: "We keep straightforward pricing based on what you opt for at our Webflow expert agency. Choose a way to work with us. No Contracts. Cancel anytime.",
   },
   {
-    image: "🚀",
+    image: css2,
     title: "Quick Delivery",
-    desc: "We deliver full-fledged Webflow websites in 2 weeks. Turnaround for hourly work is 2 days.",
+    desc: "We as a Webflow Agency deliver full-fledged Webflow website in just 2 weeks*. Our turnaround time for hourly work is just 2 days.",
   },
   {
-    image: "📜",
+    image: css1,
     title: "Certified Developers",
-    desc: "Our developers have passed all Webflow certification tests on Webflow university.",
+    desc: "We make sure that our developers have passed all Webflow certification tests on Webflow university.",
   },
   {
-    image: "✨",
+    image: css2,
     title: "Beautiful Interactions",
     desc: "We create simple and beautiful animations that improve the user's overall experience.",
   },
   {
-    image: "📱",
+    image: css1,
     title: "Responsive Design",
-    desc: "Our designs in Webflow have similar experiences across all devices.",
+    desc: "We as a Webflow design agency ensure that all the designs we develop in Webflow has similar experiences across all the devices.",
   },
   {
-    image: "⚡",
+    image: css2,
     title: "Page Speed Optimization",
-    desc: "We build with clean structure so it won’t impact speed. Google’s core web vitals stay green.",
+    desc: "We build a site with a clean structure so it won’t impact the page speed. As a Webflow development agency, we ensure that Google’s core web vitals are in green.",
   },
 ];
 
@@ -102,114 +119,204 @@ const page = () => {
   const fileMemo = useMemo(() => fileList, [fileList]);
 
   return (
-    <div className="min-h-screen bg-[#0a0a1a] bg-gradient-to-br from-[#0a0a1a] to-[#1a1a3a] text-white overflow-hidden relative">
-      <div className="absolute inset-0 z-0 mt-3">
-        {Array.from({ length: 30 }).map((_, rowIndex) =>
-          Array.from({ length: 50 }).map((_, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-              className="absolute rounded-full bg-white"
-              style={{
-                width: "2px",
-                height: "2px",
-                top: `${rowIndex * 5.5}%`,
-                left: `${colIndex * 3}%`,
-                opacity: 1,
-              }}
-            />
-          ))
-        )}
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-blue-950 pointer-events-none" />
-      </div>
-
-      <section className="py-12 md:py-20 lg:py-32 relative text-center ">
-        {/* Clutch badge */}
-        <div className="flex justify-center mt-20">
-          <div className="bg-black text-white px-3 py-1 rounded-full text-sm font-semibold flex items-center gap-2">
-            <span className="bg-white text-black rounded-full px-2 py-0.5">
-              {" xyz "}
-            </span>
-            <span>5.0 Rating</span>
-            <span className="text-yellow-300">★★★★★</span>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }} // Start with opacity 0 and move up from 50px
+      animate={{ opacity: 1, y: 0 }} // Animate to opacity 1 and y 0
+      transition={{ duration: 1, ease: "easeOut" }} // Smooth transition
+    >
+      <div className="min-h-screen bg-[#0a0a1a]  text-white overflow-hidden relative">
+        <section className="py-12 md:py-20 lg:py-32 relative text-center ">
+          {/* Clutch badge */}
+          <div className="flex justify-center mt-20">
+            <div className="bg-black text-white px-5 py-3 rounded-full text-sm font-semibold flex items-center gap-2">
+              <span className="bg-white text-black rounded-full px-3 py-0.5">
+                {" xyz "}
+              </span>
+              <span>5.0 Rating</span>
+              <span className="text-yellow-300">★★★★★</span>
+            </div>
           </div>
-        </div>
 
-        {/* Subheadline */}
-        <p className="font-medium text-lg m-3">A full-service webflow agency</p>
+          {/* Subheadline */}
+          <p className="font-medium text-lg m-3">
+            A full-service webflow agency
+          </p>
 
-        {/* Main Headline */}
-        <h1 className="text-4xl sm:text-5xl lg:text-6xl max-w-4xl mx-auto font-bold mb-6">
-          Get
-          <span className="bg-gradient-to-l from-purple-700 via-purple-400 to-blue-600 bg-clip-text text-transparent">
-            {" "}
-            high-converting
-          </span>{" "}
-          <span className="text-white">Webflow</span> Websites{" "}
-          <span className="bg-gradient-to-l from-purple-700 via-purple-400 to-blue-600 bg-clip-text text-transparent">
-            in just 2 weeks.
-          </span>
-        </h1>
+          {/* Main Headline */}
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl max-w-4xl mx-auto font-bold mb-6">
+            Get
+            <span className="bg-gradient-to-l from-purple-700 via-purple-400 to-blue-600 bg-clip-text text-transparent">
+              {" "}
+              high-converting
+            </span>{" "}
+            <span className="text-white">Webflow</span> Websites{" "}
+            <span className="bg-gradient-to-l from-purple-700 via-purple-400 to-blue-600 bg-clip-text text-transparent">
+              in just 2 weeks.
+            </span>
+          </h1>
 
-        {/* Paragraph */}
-        <p className="max-w-3xl mx-auto mb-8 text-base sm:text-lg">
-          We as a Webflow expert agency help SaaS, IT, and B2B companies get
-          more clients with Webflow websites that are optimized for more leads.
-          Our Webflow development agency provides Webflow website design,
-          Webflow development and maintenance, SEO, and Webflow migration
-          services.
-        </p>
+          {/* Paragraph */}
+          <p className="max-w-3xl mx-auto mb-8 text-base sm:text-lg">
+            We as a Webflow expert agency help SaaS, IT, and B2B companies get
+            more clients with Webflow websites that are optimized for more
+            leads. Our Webflow development agency provides Webflow website
+            design, Webflow development and maintenance, SEO, and Webflow
+            migration services.
+          </p>
 
-        {/* Call to Action */}
-        <div className="flex justify-center">
-          <button className="bg-[#6169FC] px-6 py-3 rounded-md shadow-md text-white">
-            Book a Call
-          </button>
-        </div>
+          {/* Call to Action */}
+          <div className="flex justify-center">
+            <button className="bg-[#6169FC] px-6 py-3 rounded-md shadow-md text-white">
+              Book a Call
+            </button>
+          </div>
 
-        <div className="mt-10 mb-10">
-          {/* trusted by */}
-          <HomeMarquee />
-        </div>
+          
+          <div
+            className="mb-24"
+            style={{
+              background:
+                "radial-gradient(ellipse at 50% 0%, #272b40 0%, transparent 40%)",
+            }}
+          > 
+          <div className="w-full mt-10 h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5">
+          </div>
+          <div className="bg-black"><WebflowMarquee /></div>
+          </div>
 
-        <p className="max-w-5xl mx-auto mb-8 text-5xl font-semibold">
-          5x your potential with our Range Of Webflow Development Services!
-        </p>
-        <p className="max-w-3xl mx-auto mb-8 text-base font-normal sm:text-lg">
-          Unlock your brand’s full potential with our Webflow design company,
-          offering expert solutions tailored to your needs. As a leading Webflow
-          development agency, we specialize in creating high-performing websites
-          that are not only visually stunning but also optimized for
-          conversions. Partner with us to experience a seamless blend of
-          creativity and technical expertise.
-        </p>
+          <div
+            className="mb-24"
+            style={{
+              background:
+                "radial-gradient(ellipse at 50% 0%, #272b40 0%, transparent 40%)",
+            }}
+          > 
+          <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5">
+          </div>
 
-        <div>
-          <CardMotion
-            mainHeading={""}
-            subHeading={""}
-            serviceArr={serviceArr}
-          />
-        </div>
-        <div className="flex justify-center">
-          <button className="bg-[#6169FC] px-4 py-3 rounded-md shadow-md text-white">
-            Book a Call
-          </button>
-        </div>
+          <p className="max-w-5xl mx-auto mb-8 text-5xl font-semibold mt-10">
+            5x your potential with our Range Of Webflow Development Services!
+          </p>
+          <p className="max-w-3xl mx-auto mb-8 text-base font-normal sm:text-lg">
+            Unlock your brand’s full potential with our Webflow design company,
+            offering expert solutions tailored to your needs. As a leading
+            Webflow development agency, we specialize in creating
+            high-performing websites that are not only visually stunning but
+            also optimized for conversions. Partner with us to experience a
+            seamless blend of creativity and technical expertise.
+          </p>
 
-        <p className="max-w-6xl mx-auto mb-5 text-4xl font-semibold mt-20">
-          Why you should choose us as your Webflow Partner.
-        </p>
-        <p className="max-w-3xl mx-auto mb-8 text-base font-normal sm:text-lg">
-          We have over 17+ years of experience in design and development.
-        </p>
+          <div>
+            <CardMotion
+              serviceArr={serviceArr}
+            />
+          </div>
+          <div className="flex justify-center">
+            <button className="bg-[#6169FC] px-4 py-3 rounded-md shadow-md text-white">
+              Book a Call
+            </button>
+          </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto justify-center items-center">
-          {serviceCards.map((card, index) => (
-            <CardComponent key={index} {...card} />
-          ))}
-        </div>
-      </section>
-    </div>
+          </div>
+
+          <div
+            className="mb-24"
+            style={{
+              background:
+                "radial-gradient(ellipse at 50% 0%, #272b40 0%, transparent 40%)",
+            }}
+          > 
+          <div className="w-full mt-10 h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5"></div>
+
+          <p className="max-w-6xl mx-auto mb-5 text-4xl font-semibold mt-20">
+            Why you should choose us as your Webflow Partner.
+          </p>
+          <p className="max-w-3xl mx-auto mb-8 text-base font-normal sm:text-lg">
+            We have over 17+ years of experience in design and development.
+          </p>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl justify-center mx-auto">
+            {serviceCards.map((card, index) => (
+              <CardComponent
+              key={index}
+              image={card.image}
+              title={card.title}
+              desc={card.desc}
+              />
+            ))}
+          </div>
+
+          <div className="flex justify-center">
+            <button className="bg-[#6169FC] px-4 py-3 rounded-md shadow-md text-white mt-10">
+              Book a Call
+            </button>
+          </div>
+            </div>
+
+          <div
+            className="mb-24"
+            style={{
+              background:
+                "radial-gradient(ellipse at 50% 0%, #272b40 0%, transparent 40%)",
+            }}
+          > 
+          <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5">
+          </div>
+          <AnimatedCard />
+          </div>
+
+          
+          <div
+            className="mb-24"
+            style={{
+              background:
+                "radial-gradient(ellipse at 50% 0%, #272b40 0%, transparent 40%)",
+            }}
+          > 
+          <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5">
+          </div>
+          <Webtable/>
+          </div>
+
+
+          <div
+            style={{
+              background:
+                "radial-gradient(ellipse 80% 60% at 50% 0%, #272b45 0%, transparent 40%)",
+            }}
+          >
+            <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 mb-5"></div>
+            <TestimonialSlider />
+          </div>
+
+          <div
+            style={{
+              background:
+                "radial-gradient(ellipse 80% 60% at 50% 0%, #272b45 0%, transparent 40%)",
+            }}
+          >
+            <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 mb-5"></div>
+            <ThirdPartyIntegrations />
+          </div>
+
+          <div
+            style={{
+              background:
+                "radial-gradient(ellipse 80% 60% at 50% 0%, #272b45 0%, transparent 40%)",
+            }}
+          >
+          <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 mb-5"></div>
+            <FAQ />
+          </div>
+
+          <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 "></div>
+            <div className="w-full flex justify-center items-center">
+              <BookDemo />
+            </div>
+
+        </section>
+      </div>
+    </motion.div>
   );
 };
 
