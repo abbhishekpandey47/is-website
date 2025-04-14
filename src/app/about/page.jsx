17dfb45e2a115df47dfd0f4b8d8545dcadb96f2b
Satish Kolhe <kolhesatish96@gmail.com>
2025-04-14 16:03:03 +0530

    add lines in about page-4

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -80,11 +80,13 @@ const about = () => {
       {/* Founder Section */}
       <section className="w-full px-4 md:px-12 lg:px-16  overflow-hidden relative">
         <div
-          className={`w-full h-px bg-white/30 mb-16 opacity-0 ${
-            isVisible ? "opacity-100 animate-fade-in" : ""
-          }`}
-          style={{ transitionDelay: "300ms" }}
-        ></div>
+                  style={{
+                    background: "radial-gradient(ellipse 80% 60% at 50% 0%, #272b45 0%, transparent 40%)",
+                  }}
+                  
+                >
+                  <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 mb-5"></div>
+          
 
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-16 items-start items-center ">
@@ -179,15 +181,19 @@ const about = () => {
             </div>
           </div>
         </div>
+        </div>
       </section>
       {/* Advisor Section */}
       <section className="w-full py-12 mt-5 px-6 md:px-12 lg:px-16  overflow-hidden">
         <div
-          className={`w-full h-px bg-white/30 mb-16 opacity-0 ${
-            isVisible ? "opacity-100 animate-fade-in" : ""
-          }`}
-          style={{ transitionDelay: "300ms" }}
-        ></div>
+                 style={{
+                  background: "radial-gradient(ellipse 80% 60% at 50% 0%, #272b45 0%, transparent 40%)",
+                }}
+                
+              >
+                <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 mb-5"></div>
+        
+        
 
         <div className="max-w-7xl mx-auto text-center">
           <h2
@@ -257,14 +263,18 @@ const about = () => {
             </div>
           </div>
         </div>
+        </div>
       </section>
+      {/* why infrasity section */}
       <section className="w-full  py-10 px-6 md:px-10">
         <div
-          className={`w-full h-px bg-white/30 mb-16 opacity-0 ${
-            isVisible ? "opacity-100 animate-fade-in" : ""
-          }`}
-          style={{ transitionDelay: "300ms" }}
-        ></div>
+                  style={{
+                    background: "radial-gradient(ellipse 80% 60% at 50% 0%, #272b45 0%, transparent 40%)",
+                  }}
+                  
+                >
+                  <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 mb-5"></div>
+          
         <div className="max-w-7xl mx-auto">
           <h2
             className={`text-5xl md:text-6xl quicksand-bold text-white-800 mb-20 text-center opacity-0 ${
@@ -301,26 +311,31 @@ const about = () => {
             ))}
           </div>
         </div>
+        </div>
       </section>
-      <div
-          className={`w-full h-px bg-white/30 mb-16 opacity-0 ${
-            isVisible ? "opacity-100 animate-fade-in" : ""
-          }`}
-          style={{ transitionDelay: "300ms" }}
-        ></div>
+  
 
-      <div>
+      <div 
+              style={{
+                background: "radial-gradient(ellipse 80% 60% at 50% 0%, #272b45 0%, transparent 40%)",
+              }}
+              
+            >
+              <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 mb-5"></div>
+      
         < Timeline />
       </div>
-      <div
-          className={`w-full h-px bg-white/30 mb-16 opacity-0 ${
-            isVisible ? "opacity-100 animate-fade-in" : ""
-          }`}
-          style={{ transitionDelay: "300ms" }}
-        ></div>
+      
 
-      <div className="w-full flex justify-center items-center">
+      {/* Book demo section */}
+      <div  style={{
+          background:
+            "radial-gradient(ellipse at 50% 0%, #272b40 0%, transparent 40%)",
+        }}>
+        <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 mb-12"></div>
+      <div className="w-full flex justify-center items-center"> 
         <BookDemo />
+      </div>
       </div>
     </div>
   );
