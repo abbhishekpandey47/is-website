d56dff4183ddea81bd41128e73ce28c8bfae08fd
Satish Kolhe <kolhesatish96@gmail.com>
2025-04-14 16:05:43 +0530

    add lines in about page-5

--- src/app/services/technical-writing-services/cli-guide/how-to-guides.jsx
+++ src/app/services/technical-writing-services/cli-guide/how-to-guides.jsx
@@ -87,10 +87,10 @@ const CRMAutomationPage = () => {
           background:
             "radial-gradient(ellipse at 50% 0%, #272b40 0%, transparent 40%)",
         }}>
-        <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 mb-24"></div>
+        <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 mb-16"></div>
         <div
           key={section.id}
-          className="w-full max-w-full rounded-2xl p-8 relative overflow-hidden box-border mb-16"
+          className="w-full max-w-full rounded-2xl p-8 relative overflow-hidden box-border mb-12"
           style={{
             backgroundColor: "#141318",
             backgroundImage: `radial-gradient(circle at top right, #272b40 0%, transparent 80%)`,
@@ -162,10 +162,11 @@ const CRMAutomationPage = () => {
               </div>
             ))}
           </div>
-        </div>
-        <div className="flex justify-center mb-8"> 
+          <div className="flex justify-center mb-10 mt-10"> 
         < CalendlyButton />
         </div>
+        </div>
+
         </div>
       ))}
     </div>
