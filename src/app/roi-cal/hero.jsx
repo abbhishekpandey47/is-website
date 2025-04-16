5f219ea47561d0b03b75b69d2604c2270d78b5e4
Satish Kolhe <kolhesatish96@gmail.com>
2025-04-16 20:13:14 +0530

    Chnaged design for mobile devices

--- src/app/roi-cal/hero.jsx
+++ src/app/roi-cal/hero.jsx
@@ -1,39 +1,37 @@
 import React from 'react';
 
 export default function Hero() {
-
-    const svgColor = "#afb0af";
-
+  const svgColor = "#afb0af";
+  
   return (
-    <div className="flex flex-col items-center justify-center p-8 pt-40 w-full max-w-4xl mx-auto">
-   <div className="flex flex-col items-center justify-center">
-      <div className="flex items-center justify-center">
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          className="h-5 w-5" 
-          fill="none" 
-          viewBox="0 0 24 24" 
-          stroke={svgColor}
-        >
-          <path 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
-            strokeWidth={2} 
-            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" 
-          />
-        </svg>
+    <div className="flex flex-col items-center justify-center p-4 md:p-8 pt-28 md:pt-40 w-full max-w-4xl mx-auto">
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 md:h-5 md:w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke={svgColor}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+            />
+          </svg>
+        </div>
+        <div className="flex items-center justify-center font-medium text-xs md:text-sm text-center pt-2 px-4" style={{ color: svgColor }}>
+          Content ROI Calculator for DevTool Startups, early-stage infra, AI companies
+        </div>
       </div>
-      <div className="flex items-center justify-center font-medium text-sm" style={{ color: svgColor }}>
-        ROI Calculator
-      </div>
-    </div>
       
-      <div className="text-center">
-        <h1 className="text-7xl font-semibold mt-8">
-          See how much time <br />
-          and money Fluent <br />
-          can save your team
-        </h1>
+      <div className="text-center px-4">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-semibold mt-4 md:mt-8">
+          Estimate the true cost of content and
+          see how much you save by outsourcing.
+          </h1>
       </div>
     </div>
   );
