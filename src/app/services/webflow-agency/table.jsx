d7b3288c53da05172c8c98657176971f5529b3b6
Satish Kolhe <kolhesatish96@gmail.com>
2025-04-24 15:58:13 +0530

    Fixed hero section

--- src/app/services/webflow-agency/table.jsx
+++ src/app/services/webflow-agency/table.jsx
@@ -7,12 +7,12 @@ export default function Webtable() {
   return (
     <div className="flex flex-col items-center max-w-7xl mx-auto p-2 md:p-6 py-4 mt-10 text-sm md:text-md">
       <h1 className="text-xl md:text-3xl font-bold mb-4 md:mb-8 text-center text-white pb-3 text-[16px]">
-      Why High-Growth Startups Work With Us
+        Why High-Growth Startups Work With Us
       </h1>
 
       {/* Mobile and Desktop table with horizontal scroll for small screens */}
       <div className="w-full overflow-x-auto scrollbar-hide">
-        <div className="min-w-[800px] grid grid-cols-4 gap-4 md:gap-8 text-start">
+        <div className="min-w-[800px] grid grid-cols-4 gap-4 md:gap-8 text-center">
           {/* Left Column */}
           <div
             className="flex flex-col gap-4 md:gap-6 text-center p-2 md:p-2 rounded-lg justify-start pt-3"
@@ -33,24 +33,28 @@ export default function Webtable() {
               <p className="font-bold text-white pb-3 text-[16px]">Pricing</p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center">
-              <p className="font-bold text-white pb-3 text-[16px]">Start Time</p>
-            </div>
-            <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center">
-              <p className="font-bold text-white pb-3 text-[16px]">Specialization</p>
-            </div>
-            <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center">
               <p className="font-bold text-white pb-3 text-[16px]">
-              Speed to Launch
+                Start Time
               </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center">
               <p className="font-bold text-white pb-3 text-[16px]">
-              Design Philosophy
+                Specialization
+              </p>
+            </div>
+            <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center">
+              <p className="font-bold text-white pb-3 text-[16px]">
+                Speed to Launch
+              </p>
+            </div>
+            <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center">
+              <p className="font-bold text-white pb-3 text-[16px]">
+                Design Philosophy
               </p>
             </div>
             <div className="w-full flex items-center">
               <p className="font-bold text-white pb-3 text-[16px]">
-              Collaboration Style
+                Collaboration Style
               </p>
             </div>
           </div>
@@ -75,24 +79,35 @@ export default function Webtable() {
               />
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Scope-based. Per page. No retainers.</p>
-            </div>
-            <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
               <p className="text-white pb-3 text-[16px]">
-              Start in 2 business days
+                Scope-based. Per page. No retainers.
               </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Only B2B SaaS, infra, and AI. We know what YC-style startups need to ship fast</p>
+              <p className="text-white pb-3 text-[16px]">
+                Start in 2 business days
+              </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">First draft in less than 7 days</p>
+              <p className="text-white pb-3 text-[16px]">
+                Only B2B SaaS, infra, and AI. We know what YC-style startups
+                need to ship fast
+              </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Clean. On-brand. Built to impress investors and customers.</p>
+              <p className="text-white pb-3 text-[16px]">
+                First draft in less than 7 days
+              </p>
+            </div>
+            <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
+              <p className="text-white pb-3 text-[16px]">
+                Clean. On-brand. Built to impress investors and customers.
+              </p>
             </div>
             <div className="w-full flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Slack-native. Works like an extension of your team.</p>
+              <p className="text-white pb-3 text-[16px]">
+                Slack-native. Works like an extension of your team.
+              </p>
             </div>
           </div>
 
@@ -114,13 +129,20 @@ export default function Webtable() {
               </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Unpredictable. Hourly. Scope creep.</p>
+              <p className="text-white pb-3 text-[16px]">
+                Unpredictable. Hourly. Scope creep.
+              </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">	Takes a week or more</p>
+              <p className="text-white pb-3 text-[16px]">
+                {" "}
+                Takes a week or more
+              </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Generalists. Often not startup-ready</p>
+              <p className="text-white pb-3 text-[16px]">
+                Generalists. Often not startup-ready
+              </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
               <p className="text-white pb-3 text-[16px]">Variable</p>
@@ -129,7 +151,9 @@ export default function Webtable() {
               <p className="text-white pb-3 text-[16px]">Varies widely</p>
             </div>
             <div className="w-full flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Email, DMs, WhatsApp chaos</p>
+              <p className="text-white pb-3 text-[16px]">
+                Email, DMs, WhatsApp chaos
+              </p>
             </div>
           </div>
 
@@ -152,25 +176,29 @@ export default function Webtable() {
               </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Expensive monthly retainers.</p>
+              <p className="text-white pb-3 text-[16px]">
+                Expensive monthly retainers.
+              </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
               <p className="text-white pb-3 text-[16px]">Takes 2–3 weeks</p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
               <p className="text-white pb-3 text-[16px]">
-              Often built for enterprises
+                Often built for enterprises
               </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
-              <p className="text-white pb-3 text-[16px]">Slow due to approvals and layers</p>
+              <p className="text-white pb-3 text-[16px]">
+                Slow due to approvals and layers
+              </p>
             </div>
             <div className="border-b border-solid border-gray-300 w-full pb-2 flex items-center justify-center">
               <p className="text-white pb-3 text-[16px]">Often overdesigned</p>
             </div>
             <div className="w-full flex items-center justify-center">
               <p className="text-white pb-3 text-[16px]">
-              Email chains. PM bottlenecks.
+                Email chains. PM bottlenecks.
               </p>
             </div>
           </div>
