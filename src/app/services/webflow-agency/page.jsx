5574b668dd71a215c5d7b8be6797cb897626aff8
Satish Kolhe <kolhesatish96@gmail.com>
2025-04-26 14:20:14 +0530

    Added flow Ninja clone-6

--- src/app/services/webflow-agency/page.jsx
+++ src/app/services/webflow-agency/page.jsx
@@ -204,7 +204,16 @@ const page = () => {
             <ServicesSection />
           </div>
 
-          <ScrollingServicesSection />
+          {/* <div
+            className="mb-24"
+            style={{
+              background:
+                "radial-gradient(ellipse at 50% 0%, #272b40 0%, transparent 40%)",
+            }}
+          >
+            <div className="w-full mt-10 h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5"></div>
+            <ScrollingServicesSection />
+          </div> */}
 
           {/* Services Section */}
           {/* <div
