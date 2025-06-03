2135eb8e1cec9457daf56c70b81f9af834754ec9
Satish Kolhe <kolhesatish96@gmail.com>
2025-06-03 14:37:41 +0530

    Added svg YC-PAGE- 5

---
+++ src/app/services/yc-page/svg/tool.jsx
@@ -0,0 +1,7 @@
+export default function Tool() {
+    return (
+        <svg
+            className="w-5 h-5"
+            fill="#ffa600" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="m21.512 6.112-3.89 3.889-3.535-3.536 3.889-3.889a6.501 6.501 0 0 0-8.484 8.486l-6.276 6.275a.999.999 0 0 0 0 1.414l2.122 2.122a.999.999 0 0 0 1.414 0l6.275-6.276a6.501 6.501 0 0 0 7.071-1.414 6.504 6.504 0 0 0 1.414-7.071z"></path></g></svg>
+    )
+}
