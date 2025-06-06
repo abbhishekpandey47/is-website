a17cdd0c99ce50c4a41122b4f1d66cec38a99d04
Satish Kolhe <kolhesatish96@gmail.com>
2025-06-06 22:05:58 +0530

    redirection from https://www.infrasity.com/blog/explainer-video-agency to https://www.infrasity.com/blog/saas-video-production

--- next.config.js
+++ next.config.js
@@ -1,23 +1,28 @@
 module.exports = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-  images: {
-    domains: ['www.infrasity.com', 'images.surferseo.art'],
-  },
-  // Add redirect configuration
-  async redirects() {
-    return [
-      {
-        source: '/blog/how-to-create-a-powerful-product-document',
-        destination: '/blog/b2b-saas-prd',
-        permanent: true,
-      },
-      {
-        source: '/case-studies/top-10-technical-writing-service-companies',
-        destination: '/blog/top-10-technical-writing-service-companies',
-        permanent: true,
-      },
-    ];
-  },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+    images: {
+        domains: ['www.infrasity.com', 'images.surferseo.art'],
+    },
+    // Add redirect configuration
+    async redirects() {
+        return [
+            {
+                source: '/blog/how-to-create-a-powerful-product-document',
+                destination: '/blog/b2b-saas-prd',
+                permanent: true,
+            },
+            {
+                source: '/case-studies/top-10-technical-writing-service-companies',
+                destination: '/blog/top-10-technical-writing-service-companies',
+                permanent: true,
+            },
+            {
+                source: '/blog/explainer-video-agency',
+                destination: '/blog/saas-video-production',
+                permanent: true,
+            },
+        ];
+    },
 };
