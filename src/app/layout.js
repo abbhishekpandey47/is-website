8f023436338a10eba927f5ddbd2b660c29747008
Satish Kolhe <kolhesatish96@gmail.com>
2025-06-16 12:36:17 +0530

    Added source URLs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,52 +1,59 @@
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
 import { ClientLayoutWrapper } from './ClientLayoutWrapper';
-import "./globals.css";
+import './globals.css';
 import { metadata } from './metadata';
 
 export { metadata };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet" />
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
+    return (
+        <html lang='en'>
+            <head>
+                <link rel='alternate' href='https://www.infrasity.com/' hreflang='x-default' />
+                <link rel='alternate' href='https://www.infrasity.com/' hreflang='en-us' />
+                <link rel='preconnect' href='https://fonts.googleapis.com' />
+                <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Quicksand&display=swap'
+                    rel='stylesheet'
+                />
+                <Script id='google-tag-manager' strategy='afterInteractive'>
+                    {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-WGZGHXZS');
           `}
-        </Script>
-        <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
-        <Script id="clarity-script" strategy="afterInteractive">
-          {`
+                </Script>
+                <script
+                    src='https://assets.calendly.com/assets/external/widget.js'
+                    type='text/javascript'
+                    async
+                ></script>
+                <Script id='clarity-script' strategy='afterInteractive'>
+                    {`
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "rc3czxet7l");
           `}
-        </Script>
-      </head>
-      <GoogleAnalytics gaId="G-G0BTN1FRWY"/>
-      <body className="antialiased">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WGZGHXZS"
-            height="0"
-            width="0"
-            style={{display: 'none', visibility: 'hidden'}}
-          ></iframe>
-        </noscript>
-        <ClientLayoutWrapper>
-          {children}
-        </ClientLayoutWrapper>
-      </body>
-    </html>
-  );
+                </Script>
+            </head>
+            <GoogleAnalytics gaId='G-G0BTN1FRWY' />
+            <body className='antialiased'>
+                <noscript>
+                    <iframe
+                        src='https://www.googletagmanager.com/ns.html?id=GTM-WGZGHXZS'
+                        height='0'
+                        width='0'
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
+                <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
+            </body>
+        </html>
+    );
 }
