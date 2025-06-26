0089a0c55edc12eb91677634383b5eefdb71c06f
Komal-Infrasity <komalkofficial@gmail.com>
2025-06-26 18:04:56 +0530

    Update _postMetadata.js

--- posts/_postMetadata.js
+++ posts/_postMetadata.js
@@ -861,7 +861,7 @@ let postMetaData = [
         metatitle: "5 Content Distribution Platforms For SaaS Technical Content",
         slug: "content-distribution-platforms",
         description: "Don't miss out on using these 5 content distribution platforms to promote your content and connect with your target audiences and direct them to your website.",
-        ogImage: "/PostImages/developer-experience-engineer/dp1.webp",
+        ogImage: "/PostImages/content-distribution-platforms/dp1.webp",
         publishedOn: "2025-06-26",
         authorId: "author0004",
         category: "Informational",
