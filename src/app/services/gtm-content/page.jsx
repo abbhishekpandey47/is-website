2b82bc75e44ccbd795ee99cdc26512f49a55e770
Satish Kolhe <kolhesatish96@gmail.com>
2025-07-02 11:59:18 +0530

    Added 6th section

--- src/app/services/gtm-content/page.jsx
+++ src/app/services/gtm-content/page.jsx
@@ -4,6 +4,7 @@ import Cta from "./cta";
 import WhyChoose from "./whyyc";
 import WhatWeCreate from "./whatWeCreate";
 import TechWeCover from "./techWeCover";
+import RealResult from "./realResult";
 
 export default function Page() {
     return (
@@ -12,6 +13,7 @@ export default function Page() {
             <WhyChoose />
             <WhatWeCreate />
             <TechWeCover />
+            <RealResult />
 
             <div className="w-full h-px shadow-[#877aeb] bg-gradient-to-r from-pink-500/5 via-[#877aeb] to-pink-[#877aeb] pb-[2px] mb-10"></div>
 
