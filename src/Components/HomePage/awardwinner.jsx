50196aa0b5eb1c9d9cc1caee8ae7a1852c6c123c
Satish Kolhe <kolhesatish96@gmail.com>
2025-07-26 17:52:22 +0530

    Fixed AwardBanner

--- src/Components/HomePage/awardwinner.jsx
+++ src/Components/HomePage/awardwinner.jsx
@@ -11,7 +11,7 @@ export default function AwardBanner() {
     <div className="bg-gradient-to-r from-blue-600 to-purple-600 text-white py-2 px-3 sm:px-4">
       <div>
       <div className="mx-auto flex items-center justify-center">
-        <div className="flex items-center justify-center flex-grow gap-0 sm:gap-2 text-xs sm:text-sm md:text-base -mb-4">
+        <div className="flex items-center justify-center flex-grow gap-0 sm:gap-2 text-xs sm:text-sm md:text-base -mb-2 md:-mb-4">
           <Award className="h-4 w-4 sm:h-5 sm:w-full md:h-6 md:w-6  flex-shrink-0" />
           <p className="text-center lg:text-center">
             Infrasity has been recognized as the{" "}
@@ -39,9 +39,9 @@ export default function AwardBanner() {
         </button>
         </div>
 
-<div className="relative pt-10 px-4">
-          <div className="container mx-auto flex items-center justify-center">
-            <div className="max-w-lg bg-white/15 rounded-lg border border-white/30 flex items-center justify-between gap-3 py-1 px-4 hover:bg-white/20 transition-all duration-200 group">
+<div className=" flex items-center justify-center pt-10 md:px-4">
+          <div className="lg:mx-auto flex items-center justify-center">
+            <div className="md:max-w-lg bg-white/15 rounded-lg border border-white/30 flex items-center justify-between gap-3 py-1 px-4 hover:bg-white/20 transition-all duration-200 group">
               <div className="flex items-center gap-3 cursor-pointer">
                 <div className="bg-white/20 rounded-full p-1">
                   <ArrowRight className="h-4 w-4 text-white" />
@@ -56,7 +56,7 @@ export default function AwardBanner() {
               </div>
               <button
           onClick={() => setIsVisible(false)}
-                className="text-white/60 hover:text-white transition-colors duration-200 hover:bg-white/10 rounded-full p-1"
+                className="text-white/60 hover:text-white transition-colors duration-200 hover:bg-white/10 rounded-full p-1 z-10"
               >
                 <X className="z-10" size={16} />
               </button>
