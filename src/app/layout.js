36e6ae199728734051e7a623ad3211f6209c4b3e
Satish Kolhe <kolhesatish96@gmail.com>
2025-08-02 14:04:07 +0530

    replaced reb2b code

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,9 +55,38 @@ export default function RootLayout({ children }) {
                     crossOrigin='anonymous'
                 />
 
-                <Script id="reb2b-script" strategy="afterInteractive" dangerouslySetInnerHTML={{
-                    __html: `!function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://b2bjsstore.s3.us-west-2.amazonaws.com/b/" + key + "/0NW1GHLXJDO4.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("0NW1GHLXJDO4");}();`
-                }} />
+              <Script id="reb2b-snippet" strategy="afterInteractive">
+        {`
+          !function () {
+            var reb2b = window.reb2b = window.reb2b || [];
+            if (reb2b.invoked) return;
+            reb2b.invoked = true;
+            reb2b.methods = ["identify", "collect"];
+            reb2b.factory = function (method) {
+              return function () {
+                var args = Array.prototype.slice.call(arguments);
+                args.unshift(method);
+                reb2b.push(args);
+                return reb2b;
+              };
+            };
+            for (var i = 0; i < reb2b.methods.length; i++) {
+              var key = reb2b.methods[i];
+              reb2b[key] = reb2b.factory(key);
+            }
+            reb2b.load = function (key) {
+              var script = document.createElement("script");
+              script.type = "text/javascript";
+              script.async = true;
+              script.src = "https://b2bjsstore.s3.us-west-2.amazonaws.com/b/" + key + "/VN080HXMWP6J.js.gz";
+              var first = document.getElementsByTagName("script")[0];
+              first.parentNode.insertBefore(script, first);
+            };
+            reb2b.SNIPPET_VERSION = "1.0.1";
+            reb2b.load("VN080HXMWP6J");
+          }();
+        `}
+      </Script>
 
 
                 <Script id='google-tag-manager' strategy='lazyOnload'>{`
