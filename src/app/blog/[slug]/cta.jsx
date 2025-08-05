8c62b47e5573a4a6a1a6a840539faee7405d0d36
Satish Kolhe <kolhesatish96@gmail.com>
2025-08-05 12:44:50 +0530

    Added new cta in all blogs

--- src/app/blog/[slug]/cta.jsx
+++ src/app/blog/[slug]/cta.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function CTA() {
     return (
-        <div className="max-w-7xl mx-auto px-6">
+        <div className="w-full">
             <div className="relative overflow-hidden rounded-2xl border border-white/20 backdrop-blur-sm">
                 <div className="absolute inset-0 bg-[#2a2d5a]/30 border rounded-2xl border-purple-500/30"></div>
 
-                <div className="px-8 pb-12 md:px-12 md:pb-10 text-center md:text-left">
-                    <div className="max-w-3xl mx-auto">
+                <div className="px-8 pb-12 md:px-12 md:pb-10 text-center md:text-left -mt-6">
+                    <div className="">
 
-                        <h2 className="font-[quicksand] text-4xl md:text-5xl font-bold text-white mb-6 leading-tight">
+                        <h2 className="font-[quicksand] text-3xl md:text-4xl font-bold text-white mb-6 leading-tight">
                             Tired of wasting engineering time on content?
                         </h2>
 
@@ -22,10 +22,12 @@ export default function CTA() {
 
                         <Link
                             href="/contact"
-                            className="group no-underline  inline-flex items-center gap-3 bg-gradient-to-r from-blue-700 to-purple-700 text-white font-semibold px-8 py-4 rounded-xl transition-all duration-300 transform hover:scale-105 hover:shadow-2xl hover:shadow-blue-500/25">
-                            Schedule a 15-min call
-                            <ArrowRightIcon className="h-5 w-5 transition-transform duration-300 group-hover:translate-x-1" />
+                            className="group no-underline inline-flex items-center gap-2 bg-gradient-to-r from-blue-700 to-purple-700 text-white px-4 py-3 rounded-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl hover:shadow-blue-500/25"
+                        >
+                            <span className="text-[14px]">Schedule a 15-min call</span>
+                            <ArrowRightIcon className="h-4 w-4 transition-transform duration-300 group-hover:translate-x-1" />
                         </Link>
+
                     </div>
                 </div>
 
