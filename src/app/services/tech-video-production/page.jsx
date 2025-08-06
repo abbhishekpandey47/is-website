43a72f7e5f1f605c7e3d2cdf7b9ffaf31bf56703
Satish Kolhe <kolhesatish96@gmail.com>
2025-08-06 15:00:57 +0530

    Added new People Focused Section

--- src/app/services/tech-video-production/page.jsx
+++ src/app/services/tech-video-production/page.jsx
@@ -36,6 +36,7 @@ import CalendarBooking from "../../calendarButton.jsx";
 import ClutchBadge from "./clutch.jsx";
 import Video from "./video.jsx";
 import RecentWork from "./recent-work.jsx";
+import PeopleFocused from "./peopleFocused.jsx";
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(CustomEase);
 
@@ -491,6 +492,19 @@ const page = () => {
 
           <Video />
         </div>
+
+         <div
+        className="flex flex-col gap-10"
+        style={{
+          background:
+            "radial-gradient(ellipse 80% 60% at 50% 0%, #272b45 0%, transparent 40%)",
+        }}
+      >
+        <div className="w-full h-px shadow-pink-400/50 bg-gradient-to-r from-pink-500/5 via-pink-300 to-pink-500/5 pt-0.5 mb-5"></div>
+
+          <PeopleFocused />
+        </div>
+
 
         <div
         className="flex flex-col gap-10 mb-10"
