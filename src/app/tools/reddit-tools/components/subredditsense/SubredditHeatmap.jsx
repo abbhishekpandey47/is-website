d6bdf4d1aab1b9682c9c56d733c1a1f09443f9cc
Sid-Lais <sidlais.ls@gmail.com>
2025-08-29 16:13:27 +0530

    fix subredditsense
    Signed-off-by: Sid-Lais <sidlais.ls@gmail.com>

---
+++ src/app/tools/reddit-tools/components/subredditsense/SubredditHeatmap.jsx
@@ -0,0 +1,10 @@
+
+const SubredditHeatmap = ({ data }) => {
+	return (
+		<div className="flex items-center justify-center h-64 bg-gray-900 border border-gray-700 rounded-xl">
+			<span className="text-gray-400 text-lg">Subreddit Heatmap Placeholder</span>
+		</div>
+	);
+};
+
+export default SubredditHeatmap;
