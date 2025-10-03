fcb88f36c47dff5a3bd1ae20d0c40e8a682a70f7
parv15 <parv21153111@akgec.ac.in>
2025-10-03 15:58:59 +0530

    Factors.ai and Playbook link Fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -60,45 +60,43 @@ export default function RootLayout({ children }) {
                 />
 
                 {/* Factors.ai tracker */}
-                <Script id='factors-ai' strategy='afterInteractive'>
-                    {`
-          window.faitracker = window.faitracker || function () {
-            this.q = [];
-            var t = new CustomEvent("FAITRACKER_QUEUED_EVENT");
-            return this.init = function (t, e, a) {
-              this.TOKEN = t;
-              this.INIT_PARAMS = e;
-              this.INIT_CALLBACK = a;
-              window.dispatchEvent(new CustomEvent("FAITRACKER_INIT_EVENT"));
-            }, this.call = function () {
-              var e = { k: "", a: [] };
-              if (arguments && arguments.length >= 1) {
-                for (var a = 1; a < arguments.length; a++) e.a.push(arguments[a]);
-                e.k = arguments[0];
-              }
-              this.q.push(e), window.dispatchEvent(t);
-            }, this.message = function () {
-              window.addEventListener("message", function (t) {
-                if ("faitracker" === t.data.origin) {
-                  this.call("message", t.data.type, t.data.message);
+                   <Script id="factors-analytics" strategy="afterInteractive">
+          {`
+            window.faitracker = window.faitracker || function(){
+              this.q=[];
+              var t=new CustomEvent("FAITRACKER_QUEUED_EVENT");
+              return this.init=function(t,e,a){
+                this.TOKEN=t,this.INIT_PARAMS=e,this.INIT_CALLBACK=a,
+                window.dispatchEvent(new CustomEvent("FAITRACKER_INIT_EVENT"))
+              },
+              this.call=function(){
+                var e={k:"",a:[]};
+                if(arguments&&arguments.length>=1){
+                  for(var a=1;a<arguments.length;a++)e.a.push(arguments[a]);
+                  e.k=arguments[0]
                 }
-              });
-            }, this.message(), this.init(
-              "3yuwy0lgugm0vsdnuwqtlbh7rrt4ip7k",
-              { host: "https://api.factors.ai" }
-            ), this;
-          }();
-
-          (function () {
-            var t = document.createElement("script");
-            t.type = "text/javascript";
-            t.src = "https://app.factors.ai/assets/factors.js";
-            t.async = true;
-            var d = document.getElementsByTagName("script")[0];
-            d.parentNode.insertBefore(t, d);
-          })();
-        `}
-                </Script>
+                this.q.push(e),window.dispatchEvent(t)
+              },
+              this.message=function(){
+                window.addEventListener("message",function(t){
+                  if("faitracker"===t.data.origin){
+                    this.call("message",t.data.type,t.data.message)
+                  }
+                })
+              },
+              this.message(),
+              this.init("1ykmwtoj7xhjphvpft3b3hfqynnfl1dx",{host:"https://api.factors.ai"}),
+              this
+            }(),
+            function(){
+              var t=document.createElement("script");
+              t.type="text/javascript",
+              t.src="https://app.factors.ai/assets/factors.js",
+              t.async=!0,
+              (d=document.getElementsByTagName("script")[0]).parentNode.insertBefore(t,d)
+            }();
+          `}
+        </Script>
 
                 <Script id='reb2b-snippet' strategy='afterInteractive'>
                     {`
