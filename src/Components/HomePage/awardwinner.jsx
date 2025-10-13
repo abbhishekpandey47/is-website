bc9b46331d04a3cdf2892aa87d6da92a42f8ecd1
shan <shan@shans-MacBook-Air.local>
2025-10-13 22:10:57 +0530

    Update banner text to target B2B SaaS Startups instead of AI & DevTool Startups

--- src/Components/HomePage/awardwinner.jsx
+++ src/Components/HomePage/awardwinner.jsx
@@ -19,7 +19,7 @@ export default function AwardBanner() {
               <div className="z-20">
                 <Link className="z-10" href="/playbook/developer-marketing">
                   <p className="text-sm font-medium text-white text-center cursor-pointer">
-                    Download Free Playbook: [2025]Developer Marketing Playbook for AI & DevTool Startups
+                    Download Free Playbook: [2025]Developer Marketing Playbook for B2B SaaS Startups
                   </p>
                 </Link>
               </div>
