fd3eb8e7192e3bd58c884bdeb5283d2b3f3d7c13
Parv <parv21153111@akgec.ac.in>
2025-12-23 21:56:27 +0530

    Carousel Update Services

--- src/app/services/developer-marketing-agency/marquee.jsx
+++ src/app/services/developer-marketing-agency/marquee.jsx
@@ -3,49 +3,12 @@ import React, { useMemo } from "react";
 import { Marquee } from "@devnomic/marquee";
 import "@devnomic/marquee/dist/index.css";
 import Image from "next/image";
+import { serviceClientLogo , clientPaddingMap } from "@/clients";
 
-const fileList = [
-  "aviator.png","mocha.png","cedana.png","dhiwise.png","amnic.png","oso.png","ox-sec.svg",
-  "mvp-grow.png","cerbos.png","qodo-logo.png","Codegiant.png","Scalekit-logo.png","cycloid.png",
-  "scalr.png","daytona.png","stackOne.png","DevZero.png","terrateam.png","env0-infra-1.png",
-  "tracetest.png","firefly.png","TravisCI-Full-Color.png","firstock-logo.png","vapi-logo.png",
-  "kapstan.png","Zenml.png","Kubiya.png","lovable-logo.png","Meteor-ops.png","middleware-logo.png"
-];
+const fileList = serviceClientLogo
 
 const getLogoPadding = (filename) => {
-  const paddingMap = {
-    'aviator.png': 'p-3',
-    'mocha.png': 'p-8',
-    'cedana.png': 'p-5',
-    'dhiwise.png': 'p-2',
-    'amnic.png': 'p-5 filter brightness-0 invert',
-    'mvp-grow.png': 'p-4',
-    'cerbos.png': 'p-4',
-    'qodo-logo.png': 'p-8',
-    'Codegiant.png': 'p-4',
-    'Scalekit-logo.png': 'p-4',
-    'cycloid.png': 'p-7',
-    'scalr.png': 'p-4',
-    'daytona.png': 'p-5',
-    'oso.png': 'p-12 ',
-    'ox-sec.svg': 'p-8',
-    'stackOne.png': 'p-4',
-    'DevZero.png': 'p-4',
-    'terrateam.png': 'p-4',
-    'env0-infra-1.png': 'p-6',
-    'tracetest.png': 'p-4',
-    'firefly.png': 'p-5',
-    'TravisCI-Full-Color.png': 'p-5',
-    'firstock-logo.png': 'p-5',
-    'vapi-logo.png': 'p-10',
-    'kapstan.png': 'p-4',
-    'Zenml.png': 'p-4',
-    'Kubiya.png': 'p-5',
-    'lovable-logo.png': 'p-4',
-    'Meteor-ops.png': 'p-4',
-    'middleware-logo.png': 'p-3',
-  };
-
+  const paddingMap = clientPaddingMap;
   return paddingMap[filename] || 'p-4';
 };
 
