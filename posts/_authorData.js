1b1aee4063604f1b6dfb0e36f08750605851072c
Parv <parv21153111@akgec.ac.in>
2025-12-31 11:33:47 +0530

    Blog Changes

--- posts/_authorData.js
+++ posts/_authorData.js
@@ -3,7 +3,7 @@ const authorData = [
     authorId: "author0001",
     name: "Shan",
     profilePic: "/authorImages/shan.png",
-    designation: "CEO",
+    designation: "Founder",
     about: "Founder and CEO of Infrasity, has over 10 years of industry experience. He authors top-rated courses on Udemy and Infrasity.com and specializes in helping early-stage startups with tech content and developer relations services.",
     linkedIn: "https://www.linkedin.com/in/shantanu-das-devops/"
   },
