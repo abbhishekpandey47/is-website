2b7dc709f84f8a823950b0bdf20686d733a95158
Parv <parv21153111@akgec.ac.in>
2026-02-03 16:45:36 +0530

    Final optimization: Restore TrustedSection lazy-loading for desktop score recovery, maintain mobile optimizations

--- src/app/lp/reddit-marketing-agency/page.jsx
+++ src/app/lp/reddit-marketing-agency/page.jsx
@@ -36,7 +36,8 @@ export default function Page() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preload" href="/fonts/quicksand-variable.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-        <link rel="preload" href="/reddit/hyperwise.svg" as="image" />
+        <link rel="preload" href="/reddit/hyperwise.svg" as="image" importance="high" />
+        <link rel="preload" href="/reddit/together.svg" as="image" importance="high" />
         <link rel="preload" href="/reddit/freeAudit.svg" as="image" />
       </Head>
       <RedditMarketingAgencyClient respondCaseStudy={respondCaseStudy} />
