a07e2244ecc3cea3fa7d409fb564b1b9abe6e07c
Parv <parv21153111@akgec.ac.in>
2026-02-25 00:07:07 +0530

    Update

--- src/app/services/reddit-marketing-agency/page.jsx
+++ src/app/services/reddit-marketing-agency/page.jsx
@@ -33,8 +33,8 @@ export default function RedditMarketingAgencyClient() {
       <ConversationToRevenue/>
       <ProposalShowcase />
       <SubredditSense />
-      <RedditMarketingSlide />
       <InfraMethodologyTimeline />
+      <RedditMarketingSlide />
       {/* <WhyAI /> */}
 
       <RedditServiceCards />
