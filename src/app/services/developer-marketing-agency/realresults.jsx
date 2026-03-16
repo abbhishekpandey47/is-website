ebe05a33dcf2183474b1fd55e5472bcb9b0a7e02
Parv <parv21153111@akgec.ac.in>
2026-03-17 00:11:45 +0530

    Hero and Second Fold

--- src/app/services/developer-marketing-agency/realresults.jsx
+++ src/app/services/developer-marketing-agency/realresults.jsx
@@ -1,6 +1,10 @@
-export default function ServiceHighlights() {
+import CalendarBooking from "../../calendarButton";
+import ContactPopupButton from "../../lp/reddit-marketing-agency/ContactPopupButton";
+
+export default function ServiceHighlights({ isAdsVariant = false }) {
 	const cardBgSvg = "url(\"data:image/svg+xml,%3Csvg%20width%3D%27410%27%20height%3D%27449%27%20viewBox%3D%270%200%20410%20449%27%20fill%3D%27none%27%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%3E%3Cg%20clip-path%3D%27url(%23clip0_195_6855)%27%3E%3Crect%20width%3D%27410%27%20height%3D%27449%27%20rx%3D%2722.948%27%20fill%3D%27%230E0B1B%27%2F%3E%3Cg%20opacity%3D%270.5%27%20filter%3D%27url(%23filter0_f_195_6855)%27%3E%3Crect%20width%3D%27459.819%27%20height%3D%27384.712%27%20transform%3D%27matrix(-0.79866%20-0.601782%20-0.601782%200.79866%20634.551%20289.562)%27%20fill%3D%27url(%23paint0_linear_195_6855)%27%2F%3E%3C%2Fg%3E%3C%2Fg%3E%3Cdefs%3E%3Cfilter%20id%3D%27filter0_f_195_6855%27%20x%3D%27-57.2498%27%20y%3D%27-80.1976%27%20width%3D%27784.849%27%20height%3D%27770.063%27%20filterUnits%3D%27userSpaceOnUse%27%20color-interpolation-filters%3D%27sRGB%27%3E%3CfeFlood%20flood-opacity%3D%270%27%20result%3D%27BackgroundImageFix%27%2F%3E%3CfeBlend%20mode%3D%27normal%27%20in%3D%27SourceGraphic%27%20in2%3D%27BackgroundImageFix%27%20result%3D%27shape%27%2F%3E%3CfeGaussianBlur%20stdDeviation%3D%2746.5243%27%20result%3D%27effect1_foregroundBlur_195_6855%27%2F%3E%3C%2Ffilter%3E%3ClinearGradient%20id%3D%27paint0_linear_195_6855%27%20x1%3D%27459.819%27%20y1%3D%271.69576e-05%27%20x2%3D%27-70.9646%27%20y2%3D%27196.769%27%20gradientUnits%3D%27userSpaceOnUse%27%3E%3Cstop%20stop-color%3D%27%230D0A1A%27%2F%3E%3Cstop%20offset%3D%270.716097%27%20stop-color%3D%27%23473B79%27%20stop-opacity%3D%270.8%27%2F%3E%3Cstop%20offset%3D%271%27%20stop-color%3D%27%239B91C6%27%20stop-opacity%3D%270.8%27%2F%3E%3C%2FlinearGradient%3E%3CclipPath%20id%3D%27clip0_195_6855%27%3E%3Crect%20width%3D%27410%27%20height%3D%27449%27%20rx%3D%2722.948%27%20fill%3D%27white%27%2F%3E%3C%2FclipPath%3E%3C%2Fdefs%3E%3C%2Fsvg%3E\")";
-	const cardBgSvgWide = "url(\"data:image/svg+xml,%3Csvg%20width%3D%27729%27%20height%3D%27450%27%20viewBox%3D%270%200%20729%20450%27%20fill%3D%27none%27%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%3E%3Cg%20clip-path%3D%27url(%23clip0_195_6843)%27%3E%3Crect%20width%3D%27729%27%20height%3D%27450%27%20rx%3D%2722.948%27%20fill%3D%27%230E0B1B%27%2F%3E%3Cg%20opacity%3D%270.5%27%20filter%3D%27url(%23filter0_f_195_6843)%27%3E%3Crect%20x%3D%27573.052%27%20y%3D%27230.149%27%20width%3D%27459.819%27%20height%3D%27384.712%27%20transform%3D%27rotate(143.002%20573.052%20230.149)%27%20fill%3D%27url(%23paint0_linear_195_6843)%27%2F%3E%3C%2Fg%3E%3C%2Fg%3E%3Cdefs%3E%3Cfilter%20id%3D%27filter0_f_195_6843%27%20x%3D%27-118.749%27%20y%3D%27-170.154%27%20width%3D%27784.849%27%20height%3D%27770.063%27%20filterUnits%3D%27userSpaceOnUse%27%20color-interpolation-filters%3D%27sRGB%27%3E%3CfeFlood%20flood-opacity%3D%270%27%20result%3D%27BackgroundImageFix%27%2F%3E%3CfeBlend%20mode%3D%27normal%27%20in%3D%27SourceGraphic%27%20in2%3D%27BackgroundImageFix%27%20result%3D%27shape%27%2F%3E%3CfeGaussianBlur%20stdDeviation%3D%2746.5243%27%20result%3D%27effect1_foregroundBlur_195_6843%27%2F%3E%3C%2Ffilter%3E%3ClinearGradient%20id%3D%27paint0_linear_195_6843%27%20x1%3D%271032.87%27%20y1%3D%27230.149%27%20x2%3D%27502.087%27%20y2%3D%27426.918%27%20gradientUnits%3D%27userSpaceOnUse%27%3E%3Cstop%20stop-color%3D%27%270D0A1A%27%2F%3E%3Cstop%20offset%3D%270.716097%27%20stop-color%3D%27%23473B79%27%20stop-opacity%3D%270.8%27%2F%3E%3Cstop%20offset%3D%271%27%20stop-color%3D%27%239B91C6%27%20stop-opacity%3D%270.8%27%2F%3E%3C%2FlinearGradient%3E%3CclipPath%20id%3D%27clip0_195_6843%27%3E%3Crect%20width%3D%27729%27%20height%3D%27450%27%20rx%3D%2722.948%27%20fill%3D%27white%27%2F%3E%3C%2FclipPath%3E%3C%2Fdefs%3E%3C%2Fsvg%3E\")";
+	const cardBgSvgWide = "url(\"data:image/svg+xml,%3Csvg%20width%3D%27729%27%20height%3D%27450%27%20viewBox%3D%270%200%20729%20450%27%20fill%3D%27none%27%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%3E%3Cg%20clip-path%3D%27url(%23clip0_195_6843)%27%3E%3Crect%20width%3D%27729%27%20height%3D%27450%27%20rx%3D%2722.948%27%20fill%3D%27%230E0B1B%27%2F%3E%3Cg%20opacity%3D%270.5%27%20filter%3D%27url(%23filter0_f_195_6843)%27%3E%3Crect%20x%3D%27573.052%27%20y%3D%27230.149%27%20width%3D%27459.819%27%20height%3D%27384.712%27%20transform%3D%27rotate(143.002%20573.052%20230.149)%27%20fill%3D%27url(%23paint0_linear_195_6843)%27%2F%3E%3C%2Fg%3E%3C%2Fg%3E%3Cdefs%3E%3Cfilter%20id%3D%27filter0_f_195_6843%27%20x%3D%27-118.749%27%20y%3D%27-170.154%27%20width%3D%27784.849%27%20height%3D%27770.063%27%20filterUnits%3D%27userSpaceOnUse%27%20color-interpolation-filters%3D%27sRGB%27%3E%3CfeFlood%20flood-opacity%3D%270%27%20result%3D%27BackgroundImageFix%27%2F%3E%3CfeBlend%20mode%3D%27normal%27%20in%3D%27SourceGraphic%27%20in2%3D%27BackgroundImageFix%27%20result%3D%27shape%27%2F%3E%3CfeGaussianBlur%20stdDeviation%3D%2746.5243%27%20result%3D%27effect1_foregroundBlur_195_6843%27%2F%3E%3C%2Ffilter%3E%3ClinearGradient%20id%3D%27paint0_linear_195_6843%27%20x1%3D%271032.87%27%20y1%3D%27230.149%27%20x2%3D%27502.087%27%20y2%3D%27426.918%27%20gradientUnits%3D%27userSpaceOnUse%27%3E%3Cstop%20stop-color%3D%27%230D0A1A%27%2F%3E%3Cstop%20offset%3D%270.716097%27%20stop-color%3D%27%23473B79%27%20stop-opacity%3D%270.8%27%2F%3E%3Cstop%20offset%3D%271%27%20stop-color%3D%27%239B91C6%27%20stop-opacity%3D%270.8%27%2F%3E%3C%2FlinearGradient%3E%3CclipPath%20id%3D%27clip0_195_6843)%27%3E%3Crect%20width%3D%27729%27%20height%3D%27450%27%20rx%3D%2722.948%27%20fill%3D%27white%27%2F%3E%3C%2FclipPath%3E%3C%2Fdefs%3E%3C%2Fsvg%3E\")";
+	
 	
 	const cards = [
 		{
@@ -101,6 +105,20 @@ export default function ServiceHighlights() {
 					))}
 				</div>
 			</div>
+				  {/* CTA Button */}
+						<div className="mt-12 flex justify-center">
+						  {isAdsVariant ? (
+							<ContactPopupButton
+							  buttonText="Book a Demo"
+							  width="w-48"
+							  height="h-12"
+							  textSize="text-base"
+							  textWeight="quicksand-semibold"
+							/>
+						  ) : (
+							<CalendarBooking buttonText="Book a Call" />
+						  )}
+						</div>
 		</section>
 	);
 }
